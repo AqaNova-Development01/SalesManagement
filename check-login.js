ef82775854f9e0fe8eac9d5dd27f92342da01640
AqaNova-Development01 <y-murata@aquanova.co.jp>
2026-01-27 13:20:50 +0900

    Update check-login.js

--- check-login.js
+++ check-login.js
@@ -1,7 +1,9 @@
+// check-login.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://omzcbfjtwyygvvgxsiov.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Plndzp1kowyp6OgTwTGwHA_iUIO0ae0";
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export async function requireLogin() {
